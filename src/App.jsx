2deec3422d2142f3a97d49a3a70f82d1b38b8cfb
Do <do@MBP-de-dolores.lan>
2024-02-04 19:48:20 +0100

    install correctly react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Accueil from './pages/Acceuil'
+import { Router, Routes, Route } from "react-router-dom";
+import Accueil from "./pages/Acceuil";
 import Navbar from "./components/Navbar";
 import Apropos from "./pages/Apropos";
 import Oeuvres from "./pages/Oeuvres";
 
-
 function App() {
-
-
   return (
     <>
- <Router>
- <Navbar />
+      <Router>
+        <Navbar />
 
         <Routes>
-   
           <Route path="/" element={<Accueil />} />
           <Route path="/about" element={<Apropos />} />
           <Route path="/book" element={<Oeuvres />} />
-          
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
